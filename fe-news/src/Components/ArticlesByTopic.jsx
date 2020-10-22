6374276b1fa86405b25a2c58a905266edccfc09b
MBerry97 <imatty2011@hotmail.co.uk>
2020-10-22 13:02:01 +0100

    refactored articlesByTopic component into a function

--- fe-news/src/Components/ArticlesByTopic.jsx
+++ fe-news/src/Components/ArticlesByTopic.jsx
@@ -1,31 +1,12 @@
 
-import React, { Component } from 'react';
+import React from 'react';
 import Articles from './Articles';
 
-class ArticlesByTopic extends Component {
-  state = {
-    newTopic : false
-  }
-
-
-
-componentDidUpdate(prevProps, prevState) {
-    if (prevProps.topic !== this.props.topic) {
-      // this.setState({isLoading: true})
-      
-          this.setState({
-            newTopic: !this.state.newTopic,
-            // isLoading: false,
-          });
-        
-    }
-  }
-
-  render() {
+const ArticlesByTopic = (props) => {
     return (
-        <Articles topic={this.props.topic}/>
+        <Articles topic={props.topic}/>
     );
-  }
+  
 }
 
 export default ArticlesByTopic;
