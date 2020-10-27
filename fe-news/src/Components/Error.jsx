835885655de4dadc7e1d879a8cbe1cd641ab0da0
MBerry97 <imatty2011@hotmail.co.uk>
2020-10-27 21:56:38 +0000

    error page is now more user friendly

--- fe-news/src/Components/Error.jsx
+++ fe-news/src/Components/Error.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import errorIMG from '../images/error-display-image.jpg'
 
 const ErrorDisplay = (props) => {
     return (
-      <div>
-        <p>Error {props.status}: {props.message}</p>
+      <div className='error-text'>
+        <p>Oops! That looks like an error {props.status}: {props.message}. Please return home using the icon above.</p>
+        <img src={errorIMG} alt='error' />
       </div>
     );
 }
